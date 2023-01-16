b5b9962fab763eb29c11e2c9b2a38ec4fd994a49
noleemits <stevenoleemits@gmail.com>
2023-01-15 20:25:56 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//click to see picture in picture
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+    console.log("Woops error here: ", error);
+  }
+}
+
+// On Load
+selectMediaStream();
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
